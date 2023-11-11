b1451ab43e194f66d2b83be049b2d926e0c1c8b3
illyiggles <choi.jayson@gmail.com>
2023-11-11 07:19:51 +0000

    Update 3 files

--- src/components/sections/headingPages/Testimonials.js
+++ src/components/sections/headingPages/Testimonials.js
@@ -1,34 +1,51 @@
-import Carousel from './TestimonialCarousel'
-import TestimonialCard from './CardInfo/TestimonialCard';
+import Carousel from "./TestimonialCarousel";
+import TestimonialCard from "./CardInfo/TestimonialCard";
 export default function Testimonials() {
-    return (
-        <section className="testimonials">
-            <article className="testimonials-topbar">
-                    <h1>Testimonials</h1>
-            </article>
+  return (
+    <section className="testimonials">
+      <article className="testimonials-topbar">
+        <h1>Testimonials</h1>
+      </article>
 
+      <section className="testimonials-cards">
+        <TestimonialCard
+          name="Micheal Jordan"
+          description="Best Mediterranean food ever!"
+        />
+        <TestimonialCard
+          name="Dennis Rodman"
+          description="Loved the vibes and 
+                delicious food here!"
+        />
+        <TestimonialCard
+          name="Ron Harper"
+          description="Best Mediterranean food!"
+        />
+        <TestimonialCard
+          name="Phil Jackson"
+          description="The best place to get food in town."
+        />
+        <TestimonialCard
+          name="Scottie Pippen,"
+          description="Food here hits the spot after a home game!"
+        />
+        <TestimonialCard
+          name="Horace Grant"
+          description="Food is always fresh and delicious. Love bringing the family here to eat."
+        />
+        <TestimonialCard
+          name="Steve Kerr"
+          description="Food here was always FIRE! A must if you're in town."
+        />
+        <TestimonialCard
+          name="Toni Kukoc"
+          description="Remind me of fresh Mediterranean food back in Europe."
+        />
+      </section>
 
-            <section className="testimonials-cards">
-                <TestimonialCard name="Micheal Caldwell" description="This is the best Mediterranean food that I've ever had!"/>
-                <TestimonialCard name="Alan Chen" description="My Shiba Inu, Mugi, really loved the cozy vibes and 
-                delicious food here."/>
-                <TestimonialCard name="Casey Lau" description="I've had some great Mediterranean food before, but none of them beats
-             Little Lemon in flavor and texture."/>
-                <TestimonialCard name="John Rosenblum" description="Great food, welcoming staff, cozy atmosphere. A great place to 
-                treat your kids to."/>
-                <TestimonialCard name="Jim Reynor" description="The food here really refreshed me after a late night shift
-                at the local supply depot."/>
-                <TestimonialCard name="Brian Dean" description="I came to Little Lemon after a 5 hour flight from the East Coast.
-                 The food here tasted so delicious after the trip here."/>
-                <TestimonialCard name="Tyler Tohmine" description="The food here was fire!! Everyone should try this place
-                 out at least once if they live in Chicago."/>
-                <TestimonialCard name="Jack Hu" description="This restaurant served as a perfect dinner for me after a 
-                long night of studying. I would definitely order from here again!"/>
-            </section>
-
-            <section className="testimonials-carousel">
-                <Carousel />
-            </section>
-        </section>
-    );
+      <section className="testimonials-carousel">
+        <Carousel />
+      </section>
+    </section>
+  );
 }
